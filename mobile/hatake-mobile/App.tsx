42d659c9690ed3a4acf2d8836aefef181fd03080
ewilliamhe <ewilliamhe@gmail.com>
2026-02-21 14:40:15 +0000

    auto-commit for f98bc259-da39-4c16-8c7c-1817587ad019

--- mobile/hatake-mobile/App.tsx
+++ mobile/hatake-mobile/App.tsx
@@ -1,28 +1,19 @@
-import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
-import { useStore } from './src/store';
 
 export default function App() {
-  const { restoreSession } = useStore();
-
-  useEffect(() => {
-    restoreSession();
-  }, []);
-
   return (
-    <GestureHandlerRootView style={styles.container}>
+    <View style={styles.container}>
       <SafeAreaProvider>
         <NavigationContainer>
           <AppNavigator />
           <StatusBar style="auto" />
         </NavigationContainer>
       </SafeAreaProvider>
-    </GestureHandlerRootView>
+    </View>
   );
 }
 
