5c1be81ea424745b9c3080977cfbe5a7931fe6ec
ewilliamhe <ewilliamhe@gmail.com>
2026-02-22 02:18:06 +0000

    auto-commit for 0ea074a7-9d09-4dba-a32b-4fb51a75931a

--- mobile/hatake-mobile/src/components/DrawerMenu.tsx
+++ mobile/hatake-mobile/src/components/DrawerMenu.tsx
@@ -12,6 +12,7 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
+import { useTheme } from '../context/ThemeContext';
 
 const logoImage = require('../../assets/icon.png');
 
