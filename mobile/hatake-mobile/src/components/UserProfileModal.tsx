96aa7cddf79e94ade027c80739d9da40939af738
ewilliamhe <ewilliamhe@gmail.com>
2026-02-22 03:11:58 +0000

    auto-commit for 028f6c93-5bd4-48b1-b79a-e35453ba4f42

--- mobile/hatake-mobile/src/components/UserProfileModal.tsx
+++ mobile/hatake-mobile/src/components/UserProfileModal.tsx
@@ -14,6 +14,7 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import { API_URL } from '../config';
+import { useTheme } from '../context/ThemeContext';
 
 interface UserProfile {
   user_id: string;
