3f80812c98fc901dca6cc66d315f8ce0af24faf3
ewilliamhe <ewilliamhe@gmail.com>
2026-02-22 01:53:37 +0000

    auto-commit for ec354721-f451-4456-804d-0c04f0486a34

--- mobile/hatake-mobile/src/screens/WishlistScreen.tsx
+++ mobile/hatake-mobile/src/screens/WishlistScreen.tsx
@@ -16,6 +16,7 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import { API_URL } from '../config';
+import { useTheme } from '../context/ThemeContext';
 
 interface Wishlist {
   wishlist_id: string;
