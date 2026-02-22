a79f1af0cee4c8f99273c5b24e738259d83d312c
ewilliamhe <ewilliamhe@gmail.com>
2026-02-22 03:15:35 +0000

    auto-commit for 62b0b641-281f-42d7-9bf1-bb71c7ce26b4

--- mobile/hatake-mobile/App.tsx
+++ mobile/hatake-mobile/App.tsx
@@ -25,6 +25,7 @@ import SettingsScreen from './src/screens/SettingsScreen';
 import DrawerMenu from './src/components/DrawerMenu';
 import IncomingCallNotification from './src/components/IncomingCallNotification';
 import MessengerWidget from './src/components/MessengerWidget';
+import OnboardingTour, { shouldShowOnboarding } from './src/components/OnboardingTour';
 import { ThemeProvider, useTheme } from './src/context/ThemeContext';
 import { API_URL } from './src/config';
 
