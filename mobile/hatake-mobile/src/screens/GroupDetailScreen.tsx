21f5e84a1ff4e587ce2454efd6c9e369e176df1c
ewilliamhe <ewilliamhe@gmail.com>
2026-02-22 03:07:55 +0000

    auto-commit for 9a42ddf1-97e2-444b-a608-87eccfbe5345

--- mobile/hatake-mobile/src/screens/GroupDetailScreen.tsx
+++ mobile/hatake-mobile/src/screens/GroupDetailScreen.tsx
@@ -15,6 +15,7 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import { API_URL } from '../config';
+import { useTheme } from '../context/ThemeContext';
 import GroupChatScreen from './GroupChatScreen';
 
 const Container = Platform.OS === 'web' ? View : SafeAreaView;
