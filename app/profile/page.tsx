9fdffa181d3ca263319a7304aa8584e698f63472
ewilliamhe <ewilliamhe@gmail.com>
2026-02-22 04:02:48 +0000

    auto-commit for 0f7f2c25-ee69-4544-9e4b-0f2a8c33e2d4

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Navbar from '@/components/Navbar';
-import { User, Package, ShoppingBag, Users, Settings, Camera, Save, X, Edit, CreditCard, MapPin, Image as ImageIcon } from 'lucide-react';
+import { User, Package, ShoppingBag, Users, Settings, Camera, Save, X, Edit, CreditCard, MapPin, Image as ImageIcon, Award, Shield, Star, Trophy, Flame, Zap } from 'lucide-react';
 import Image from 'next/image';
 
 interface UserProfile {
