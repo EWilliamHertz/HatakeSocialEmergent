7103f84904d201e3fe57d9736b2ab85e2efac456
ewilliamhertz <ewilliamhe@gmail.com>
2026-02-26 05:32:27 +0000

    changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,60 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MessengerWidget from "@/components/MessengerWidget";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { AuthPromptProvider } from "@/components/AuthPromptModal";
-import { Toaster } from "react-hot-toast";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Hatake.Social - Trade & Collect",
-  description: "Your ultimate TCG trading and collection platform",
-  icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: '256x256', type: 'image/x-icon' },
-      { url: 'https://i.imgur.com/B06rBhI.png', sizes: '512x512', type: 'image/png' },
-    ],
-    apple: [
-      { url: 'https://i.imgur.com/B06rBhI.png', sizes: '180x180', type: 'image/png' },
-    ],
-  },
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100`}
-      >
-        <ThemeProvider>
-          <AuthPromptProvider>
-            {children}
-            <MessengerWidget />
-            <Toaster 
-              position="bottom-right"
-              toastOptions={{
-                className: 'dark:bg-gray-800 dark:text-white',
-              }}
-            />
-          </AuthPromptProvider>
-        </ThemeProvider>
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import MessengerWidget from "@/components/MessengerWidget";
-import { ThemeProvider } from "@/components/ThemeProvider";
 import GlobalCallListener from "@/components/VideoCall/GlobalCallListener";
 
 const geistSans = Geist({
